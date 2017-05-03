482ffc6772d7752b94725e0efbbf1426e341bc9f
Sandro Alvares <sandro.rider@gmail.com>
2017-05-03 11:37:52 -0300

    Update PixLR Editor - Ads Removed.user.js

--- PixLR Editor - Ads Removed.user.js
+++ PixLR Editor - Ads Removed.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PixLR Editor - Ads Removed
 // @namespace    http://www.sandroalvares.com.br
-// @version      0.2
+// @version      0.21
 // @description  PixLR Editor - Ads Removed
 // @author       Sandro Alvares
 // @match        https://pixlr.com/editor/
@@ -9,9 +9,10 @@
 // ==/UserScript==
 
 (function() {
-	setTimeout(function() {
-		if ($('body').css('padding-right')) {
-			$('body').css('padding-right', 'initial');
-		}
-	}, 1000);
+    $('body').removeProp();
+    setTimeout(function() {
+	if ($('body').css('padding-right')) {
+		$('body').css('padding-right', 'initial');
+	}
+    }, 1000);
 })();
