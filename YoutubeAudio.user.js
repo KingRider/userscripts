b118e2664d271f6b44314d918c545386d82b9934
Sandro Alvares <sandro.fernandes@mega.com.br>
2017-05-22 08:35:42 -0300

    new change element to fix hidden (NSFW)

--- YoutubeAudio.user.js
+++ YoutubeAudio.user.js
@@ -1,52 +1,50 @@
 // ==UserScript==
 // @name         YoutubeAudio
 // @namespace    http://sandroalvares.com.br
-// @version      v1.31
+// @version      v2.0
 // @description  Youtube Audio without video
 // @author       KingRider
 // @match        https://*.youtube.com/watch*
 // @match        http://*.youtube.com/watch*
 // @require      http://code.jquery.com/jquery-1.9.1.min.js
-// @updateURL    https://github.com/KingRider/userscripts/raw/master/Google%20Anti-SEO%20Links.user.js
-// @downloadURL  https://github.com/KingRider/userscripts/raw/master/Google%20Anti-SEO%20Links.user.js
+// @updateURL    https://github.com/KingRider/userscripts/raw/master/YoutubeAudio.user.js
+// @downloadURL  https://github.com/KingRider/userscripts/raw/master/YoutubeAudio.user.js
 // ==/UserScript==
 
 (function() {
     //(window.getComputedStyle(savideo, null).getPropertyValue('visibility') == 'visible')
 
+    //-- LOOP
     var removervideo = setInterval(function() { satela(); clearInterval(removervideo); }, 1000);
+
+    //-- KEY Q
     $(window).bind('keypress', function(e) {
         var code = e.keyCode || e.which;
         var control = e.ctrlKey || e.metaKey;
         if (code == 17 && control) { satela(); }
     });
 
+    //-- HIDE ADS & VIDEO
     function satela() {
-        var savideo1 = document.querySelectorAll('#player');
-        for (vd1=0;vd1 < savideo1.length; vd1++) {
-            if (savideo1[vd1].style.visibility != 'hidden') {
-                savideo1[vd1].style.visibility = 'hidden';
+        sahide(document.querySelectorAll('.player-api'));
+        sahide(document.querySelectorAll('#player'));
+        sahide(document.querySelectorAll('#movie_player'));
+        sahide(document.querySelectorAll('div[id*="related"]'));
+        sahide(document.querySelectorAll('div[id*="sidebar-contents"]'));
+    }
+
+    //-- FUNC HIDDEN
+    function sahide(elem) {
+        for (sah=0; sah < elem.length; sah++) {
+            if (elem[sah]) {
+                if (elem[sah].style.visibility != 'hidden') {
+                    elem[sah].style.visibility = 'hidden';
+                } else {
+                    elem[sah].style.visibility = '';
+                }
             } else {
-                savideo1[vd1].style.visibility = '';
+                console.log("Erro: " + elem);
             }
         }
-        var savideo2 = document.querySelectorAll('#movie_player');
-        for (vd2=0;vd2 < savideo2.length; vd2++) {
-            if (savideo2[vd2].style.visibility != 'hidden') {
-                savideo2[vd2].style.visibility = 'hidden';
-            } else {
-                savideo2[vd2].style.visibility = '';
-            }
-        }
-        if (document.getElementById('related').style.visibility != 'hidden') {
-            document.getElementById('related').style.visibility = 'hidden';
-        } else {
-            document.getElementById('related').style.visibility = '';
-        }
-        /*if (document.getElementById('watch7-container').style.visibility != 'hidden') {
-            document.getElementById('watch7-container').style.visibility = 'hidden';
-        } else {
-            document.getElementById('watch7-container').style.visibility = '';
-        }*/
     }
 })();
