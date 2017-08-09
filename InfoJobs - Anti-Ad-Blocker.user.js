76c659aa2ce771d081305ae676388a46d87a7bc0
Sandro Alvares <sandro.rider@gmail.com>
2017-08-09 12:21:25 -0300

    Update InfoJobs - Anti-Ad-Blocker.user.js

--- InfoJobs - Anti-Ad-Blocker.user.js
+++ InfoJobs - Anti-Ad-Blocker.user.js
@@ -1,11 +1,13 @@
 // ==UserScript==
 // @name         InfoJobs - Anti-Ad-Blocker
 // @namespace    http://sandroalvares.com.br
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       KingRider 2016-2017
 // @match        http://*.infojobs.com.br/*
 // @match        https://*.infojobs.com.br/*
+// @exclude      http://*.infojobs.com.br/candidate/match/insert2.aspx?iv=*
+// @exclude      https://*.infojobs.com.br/candidate/match/insert2.aspx?iv=*
 // @require      https://code.jquery.com/jquery-1.9.1.min.js
 // @grant        none
 // ==/UserScript==
