e1281c6c24ac0e8c089c78fc69617b9549a87723
Sandro Alvares <sandro.rider@gmail.com>
2020-01-22 17:50:19 -0300

    Update Active Scroll - Press F2.user.js

--- Active Scroll - Press F2.user.js
+++ Active Scroll - Press F2.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Active Scroll - Press F2
 // @namespace    http://sandroalvares.com.br
-// @version      v2.420
+// @version      v2.421
 // @description  Active Scroll a press key F2 - Update 27/Dez/2019
 // @author       Sandro Alvares (KingRider)
 // @connect      *
@@ -48,6 +48,7 @@
 // @exclude      http*://*.pixlr.com/e*
 // @exclude      http*://docs.google.com/*
 // @exclude      https://*.projectgenesis.com/*
+// @exclude      https://noticias.band.uol.com.br/*
 
 // @xrequire     http*://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
 // @grant        none
