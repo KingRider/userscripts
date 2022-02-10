f7ca3c0fdad56b98f6db0714865bad380052caf2
Sandro Alvares <sandro.rider@gmail.com>
2022-02-10 19:22:11 -0300

    Update PixLR Editor - Ads Removed.user.js

--- PixLR Editor - Ads Removed.user.js
+++ PixLR Editor - Ads Removed.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name         PixLR Editor - Ads Removed
 // @namespace    http://www.sandroalvares.com.br
-// @version      0.26
-// @date         07-nov-2019
+// @version      0.35
 // @description  PixLR Editor - Ads Removed
 // @author       Sandro Alvares
-// @match        https://pixlr.com/editor/
+// @match        http*://pixlr.com/br/e/
 // @grant        none
 
 // @updateURL    https://github.com/KingRider/userscripts/raw/master/PixLR%20Editor%20-%20Ads%20Removed.user.js
@@ -14,12 +13,19 @@
 // ==/UserScript==
 
 (function() {
-	$('body').removeProp();
-	setTimeout(function() {
-		if ($('body').css('padding-right')) {
+	//$('body').removeProp();
+	var tempo = setInterval(function() {
+		/*
+        if ($('body').css('padding-right')) {
 			$('body').css('padding-right', 'initial').css('min-height','0px').css('margin','0');
 			$('div[class^=ad]').remove();
 		}
-		if ($('iframe').length > 0) { $('iframe').remove(); }
+        */
+        if (document.querySelectorAll('div#right\-space').length > 0) {
+            document.querySelectorAll('div#right\-space')[0].style.display = 'none';
+            document.querySelectorAll('body div#workspace')[0].style.right = 0;
+            clearInterval(tempo);
+        }
+		//if ($('iframe').length > 0) { $('iframe').remove(); }
 	}, 1000);
 })();
