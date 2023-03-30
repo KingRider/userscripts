d91e2a71d648f8460eff7a82f667dca3133ade59
Sandro Alvares <sandro.rider@gmail.com>
2023-03-30 15:33:21 -0300

    Update Anti Adblock Detector.user.js

--- Anti Adblock Detector.user.js
+++ Anti Adblock Detector.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Anti Adblock Detector
 // @namespace    http://sandroalvares.com.br
-// @version      0.35
+// @version      0.38
 // @description  So you can continue browsing ad-free
 // @author       KingRider
 // @match        http*://*/*
 // @exclude      http*://*.google.com/*
-// @exclude      http*://*.playplus.com/live/*
+// @exclude      http*://*.github.com/*
+// @exclude      http*://*.youtube.com/*
+// @exclude      http*://*.playplus.com/*
 // @grant        none
 
 // @updateURL     https://github.com/KingRider/userscripts/raw/master/Anti%20Adblock%20Detector.user.js
@@ -36,6 +38,13 @@
             if (document.querySelectorAll('h2')[adb2]) {
                 if (document.querySelectorAll('h2')[adb2].innerText.indexOf('Adblock Detected') >= 0 && document.querySelectorAll('h2')[adb2].parentElement.parentElement.parentElement.parentElement.style.display != 'none') {
                     document.querySelectorAll('h2')[adb2].parentElement.parentElement.parentElement.parentElement.style.display = 'none';
+                }
+            }
+        }
+        for (var adb3 = 0; adb3 < document.querySelectorAll('span').length; adb3++) {
+            if (document.querySelectorAll('span')[adb3]) {
+                if (document.querySelectorAll('span')[adb3].innerText.indexOf('ncios detectado') >= 0 && document.querySelectorAll('span')[adb3].parentElement.parentElement.parentElement.parentElement.style.display != 'none') {
+                    document.querySelectorAll('span')[adb3].parentElement.parentElement.parentElement.parentElement.parentElement.style.display = 'none';
                 }
             }
         }
