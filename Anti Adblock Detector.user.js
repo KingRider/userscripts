df811098d9f5953b660d6613c8ed1901ddee0a44
Sandro Alvares <sandro.rider@gmail.com>
2023-08-16 08:18:11 -0300

    Update Anti Adblock Detector.user.js

--- Anti Adblock Detector.user.js
+++ Anti Adblock Detector.user.js
@@ -1,19 +1,21 @@
 // ==UserScript==
 // @name         Anti Adblock Detector
 // @namespace    http://sandroalvares.com.br
-// @version      0.462
+// @version      0.470
 // @description  So you can continue browsing ad-free
 // @author       KingRider
 // @match        http*://*/*
 // @exclude      http*://*.google.com/*
 // @exclude      http*://github.com/*
-// @exclude      http*://*.youtube.com/*
 // @exclude      http*://*.playplus.com/*
 // @exclude      http*://twitter.com/*
 // @exclude      http*://*.ultimatix.*/*
 // @exclude      http*://legacygames.com/*
 // @exclude      http*://xvideos.com/*
 // @exclude      http*://*.gov.br/*
+// @exclude      http*://*.adblockplus.org/*
+// @exclude      http*://*.alienwarearena.com/*
+// @exclude      http*://greasyfork.org/*
 // @grant        none
 
 // @updateURL     https://github.com/KingRider/userscripts/raw/master/Anti%20Adblock%20Detector.user.js
@@ -34,23 +36,27 @@
     var tempo_anti = setInterval(function() {
 
         // TVfree
-        for (var adb1 = 0; adb1 < document.querySelectorAll('h3').length; adb1++) {
-            if (document.querySelectorAll('h3')[adb1]) {
-                if (document.querySelectorAll('h3')[adb1].innerText.indexOf('Adblock Detected') > 0 && document.querySelectorAll('h3')[adb1].parentElement.parentElement.parentElement.style.display != 'none') {
-                    document.querySelectorAll('h3')[adb1].parentElement.parentElement.parentElement.style.display = 'none';
-                    console.log('adb1');
-                    quebrascroll();
+        if (window.location.href.indexOf('tvfree') > 0) {
+            for (var adb1 = 0; adb1 < document.querySelectorAll('h3').length; adb1++) {
+                if (document.querySelectorAll('h3')[adb1]) {
+                    if (document.querySelectorAll('h3')[adb1].innerText.indexOf('Adblock Detected') > 0 && document.querySelectorAll('h3')[adb1].parentElement.parentElement.parentElement.style.display != 'none') {
+                        document.querySelectorAll('h3')[adb1].parentElement.parentElement.parentElement.style.display = 'none';
+                        console.log('adb1');
+                        quebrascroll();
+                    }
                 }
             }
         }
 
         // https://megacanaisonline.me/
-        for (var adb2 = 0; adb2 < document.querySelectorAll('h2').length; adb2++) {
-            if (document.querySelectorAll('h2')[adb2]) {
-                if (document.querySelectorAll('h2')[adb2].innerText.indexOf('Adblock Detected') > 0 && document.querySelectorAll('h2')[adb2].parentElement.parentElement.parentElement.parentElement.style.display != 'none') {
-                    document.querySelectorAll('h2')[adb2].parentElement.parentElement.style.display = 'none';
-                    console.log('adb2');
-                    quebrascroll();
+        if (window.location.href.indexOf('megacanaisonline') > 0) {
+            for (var adb2 = 0; adb2 < document.querySelectorAll('h2').length; adb2++) {
+                if (document.querySelectorAll('h2')[adb2]) {
+                    if (document.querySelectorAll('h2')[adb2].innerText.indexOf('Adblock Detected') > 0 && document.querySelectorAll('h2')[adb2].parentElement.parentElement.parentElement.parentElement.style.display != 'none') {
+                        document.querySelectorAll('h2')[adb2].parentElement.parentElement.style.display = 'none';
+                        console.log('adb2');
+                        quebrascroll();
+                    }
                 }
             }
         }
@@ -78,56 +84,67 @@
             }
         }
 
-        // Project mdpDeblocker
-        function check() {};
-        function requestAnimationFrame() {};
-        function devtoolIsOpening() {};
-        document.body.style.filter = '';
-        document.body.style.webkitFilter = '';
-        for (var sscript = 0; document.querySelectorAll('script').length > sscript; sscript++) {
-            if (document.querySelectorAll('script')[sscript].innerText.indexOf('Anti-adblock') > 0) {
-                console.log('sscript1');
-                document.querySelectorAll('script')[sscript].remove();
-            }
-            if (document.querySelectorAll('script')[sscript].innerText.indexOf('|Adblock') > 0) {
-                console.log('sscript2');
-                document.querySelectorAll('script')[sscript].remove();
+        // Youtube.com
+        if (window.location.href.indexOf('youtube.com') > 0) {
+            if (document.getElementsByClassName("style-scope ytd-popup-container").length >= 0) {
+                if (document.getElementsByClassName("style-scope ytd-popup-container")[0].style.length >= 1) {
+                    document.getElementsByClassName("style-scope ytd-popup-container")[0].remove();
+                }
             }
         }
-        for (var sstyle = 0; document.querySelectorAll('style').length > sstyle; sstyle++) {
-            if (document.querySelectorAll('style')[sstyle].innerText.indexOf('Anti-adblock') > 0) {
-                console.log('sstyle1', sstyle);
-                document.querySelectorAll('style')[sstyle].remove();
+
+        // Project mdpDeblocker (não utilize youtube.com)
+        if (window.location.href.indexOf('youtube.com') == "-1") {
+
+            function check() {};
+            function requestAnimationFrame() {};
+            function devtoolIsOpening() {};
+            document.body.style.filter = '';
+            document.body.style.webkitFilter = '';
+            for (var sscript = 0; document.querySelectorAll('script').length > sscript; sscript++) {
+                if (document.querySelectorAll('script')[sscript].innerText.indexOf('Anti-adblock') > 0) {
+                    console.log('sscript1');
+                    document.querySelectorAll('script')[sscript].remove();
+                }
+                if (document.querySelectorAll('script')[sscript].innerText.indexOf('|Adblock') > 0) {
+                    console.log('sscript2');
+                    document.querySelectorAll('script')[sscript].remove();
+                }
             }
-            if (document.querySelectorAll('style')[sstyle].innerText.indexOf('blur') > 0) {
-                console.log('sstyle2', sstyle);
-                document.querySelectorAll('style')[sstyle].innerText = document.querySelectorAll('style')[sstyle].innerText.replaceAll('blur', 'xlur');
-                //document.querySelectorAll('style')[sstyle].remove();
+            for (var sstyle = 0; document.querySelectorAll('style').length > sstyle; sstyle++) {
+                if (document.querySelectorAll('style')[sstyle].innerText.indexOf('Anti-adblock') > 0) {
+                    console.log('sstyle1', sstyle);
+                    document.querySelectorAll('style')[sstyle].remove();
+                }
+                if (document.querySelectorAll('style')[sstyle].innerText.indexOf('blur') > 0) {
+                    console.log('sstyle2', sstyle);
+                    document.querySelectorAll('style')[sstyle].innerText = document.querySelectorAll('style')[sstyle].innerText.replaceAll('blur', 'xlur');
+                    //document.querySelectorAll('style')[sstyle].remove();
+                }
             }
-        }
-        for (var blur = 0; document.querySelectorAll('div').length > blur; blur++) {
-            if (document.querySelectorAll('div')[blur].style.filter !== '') {
-                document.querySelectorAll('div')[blur].style.filter = '';
-                document.querySelectorAll('div')[blur].style.webkitFilter = '';
-                console.log('blur');
+            for (var blur = 0; document.querySelectorAll('div').length > blur; blur++) {
+                if (document.querySelectorAll('div')[blur].style.filter !== '') {
+                    document.querySelectorAll('div')[blur].style.filter = '';
+                    document.querySelectorAll('div')[blur].style.webkitFilter = '';
+                    console.log('blur');
+                }
             }
-        }
-        for (var blur2 = 0; document.querySelectorAll('span').length > blur2; blur2++) {
-            if (document.querySelectorAll('span')[blur2].style.filter !== '') {
-                document.querySelectorAll('span')[blur2].style.filter = '';
-                document.querySelectorAll('span')[blur2].style.webkitFilter = '';
-                console.log('blur2');
+            for (var blur2 = 0; document.querySelectorAll('span').length > blur2; blur2++) {
+                if (document.querySelectorAll('span')[blur2].style.filter !== '') {
+                    document.querySelectorAll('span')[blur2].style.filter = '';
+                    document.querySelectorAll('span')[blur2].style.webkitFilter = '';
+                    console.log('blur2');
+                }
             }
-        }
-        for (var blur3 = 0; document.querySelectorAll('p').length > blur3; blur3++) {
-            if (document.querySelectorAll('p')[blur3].style.filter !== '') {
-                document.querySelectorAll('p')[blur3].style.filter = '';
-                document.querySelectorAll('p')[blur3].style.webkitFilter = '';
-                console.log('blur3');
+            for (var blur3 = 0; document.querySelectorAll('p').length > blur3; blur3++) {
+                if (document.querySelectorAll('p')[blur3].style.filter !== '') {
+                    document.querySelectorAll('p')[blur3].style.filter = '';
+                    document.querySelectorAll('p')[blur3].style.webkitFilter = '';
+                    console.log('blur3');
+                }
             }
-        }
-        //botao X
-        /*
+            //botao X
+            /*
         for (var ads = 0; document.querySelectorAll('[id*="ads-"]').length > ads; ads++) {
             if (document.querySelectorAll('[id*="ads-"]')[ads] !== '') {
                 //document.querySelectorAll('[id*="ads-"]')[ads].parentElement.remove();
@@ -136,24 +153,26 @@
             }
         }
         */
-        /*
+            /*
         if (document.querySelector('style#mdpDeblocker-css')) {
             console.log('style#mdp');
             document.querySelector('style#mdpDeblocker-css').remove();
         }
         */
-        for (var mdp = 0; document.querySelectorAll('style[id*="mdpDeblocker"]').length > mdp; mdp++) {
-            if (document.querySelectorAll('style[id*="mdpDeblocker"]')[mdp] !== '') {
-                console.log('mdp');
-                document.querySelectorAll('style[id*="mdpDeblocker"]')[mdp].remove();
+            for (var mdp = 0; document.querySelectorAll('style[id*="mdpDeblocker"]').length > mdp; mdp++) {
+                if (document.querySelectorAll('style[id*="mdpDeblocker"]')[mdp] !== '') {
+                    console.log('mdp');
+                    document.querySelectorAll('style[id*="mdpDeblocker"]')[mdp].remove();
+                }
+            }
+            for (var mdp2 = 0; document.querySelectorAll('style[class*="mdpDeblocker"]').length > mdp2; mdp2++) {
+                if (document.querySelectorAll('style[class*="mdpDeblocker"]')[mdp2] !== '') {
+                    console.log('mdp2');
+                    document.querySelectorAll('style[class*="mdpDeblocker"]')[mdp2].remove();
+                }
             }
         }
-        for (var mdp2 = 0; document.querySelectorAll('style[class*="mdpDeblocker"]').length > mdp2; mdp2++) {
-            if (document.querySelectorAll('style[class*="mdpDeblocker"]')[mdp2] !== '') {
-                console.log('mdp2');
-                document.querySelectorAll('style[class*="mdpDeblocker"]')[mdp2].remove();
-            }
-        }
+
 
         function quebrascroll() {
             var style=document.createElement('style');
