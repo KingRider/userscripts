518c5f78ee53563c0896ff090c47ce3d88e88b58
Sandro Alvares <sandro.rider@gmail.com>
2023-09-17 15:43:27 -0300

    Update Anti Adblock Detector.user.js

--- Anti Adblock Detector.user.js
+++ Anti Adblock Detector.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @name         Anti Adblock Detector
 // @namespace    http://sandroalvares.com.br
-// @version      0.470
+// @version      0.473
 // @description  So you can continue browsing ad-free
 // @author       KingRider
+
 // @match        http*://*/*
+
 // @exclude      http*://*.google.com/*
 // @exclude      http*://github.com/*
 // @exclude      http*://*.playplus.com/*
@@ -16,6 +18,8 @@
 // @exclude      http*://*.adblockplus.org/*
 // @exclude      http*://*.alienwarearena.com/*
 // @exclude      http*://greasyfork.org/*
+// @exclude      http*://*.amazon.com*/*
+
 // @grant        none
 
 // @updateURL     https://github.com/KingRider/userscripts/raw/master/Anti%20Adblock%20Detector.user.js
@@ -39,7 +43,7 @@
         if (window.location.href.indexOf('tvfree') > 0) {
             for (var adb1 = 0; adb1 < document.querySelectorAll('h3').length; adb1++) {
                 if (document.querySelectorAll('h3')[adb1]) {
-                    if (document.querySelectorAll('h3')[adb1].innerText.indexOf('Adblock Detected') > 0 && document.querySelectorAll('h3')[adb1].parentElement.parentElement.parentElement.style.display != 'none') {
+                    if (document.querySelectorAll('h3')[adb1].innerText.toLowerCase().indexOf('adblock detected') > 0 && document.querySelectorAll('h3')[adb1].parentElement.parentElement.parentElement.style.display != 'none') {
                         document.querySelectorAll('h3')[adb1].parentElement.parentElement.parentElement.style.display = 'none';
                         console.log('adb1');
                         quebrascroll();
@@ -52,7 +56,7 @@
         if (window.location.href.indexOf('megacanaisonline') > 0) {
             for (var adb2 = 0; adb2 < document.querySelectorAll('h2').length; adb2++) {
                 if (document.querySelectorAll('h2')[adb2]) {
-                    if (document.querySelectorAll('h2')[adb2].innerText.indexOf('Adblock Detected') > 0 && document.querySelectorAll('h2')[adb2].parentElement.parentElement.parentElement.parentElement.style.display != 'none') {
+                    if (document.querySelectorAll('h2')[adb2].innerText.toLowerCase().indexOf('adblock detected') > 0 && document.querySelectorAll('h2')[adb2].parentElement.parentElement.parentElement.parentElement.style.display != 'none') {
                         document.querySelectorAll('h2')[adb2].parentElement.parentElement.style.display = 'none';
                         console.log('adb2');
                         quebrascroll();
@@ -65,7 +69,7 @@
         // dailymotion (não funcionado desbloquear pop e estou trabalhando)
         for (var adb3 = 0; adb3 < document.querySelectorAll('span').length; adb3++) {
             if (document.querySelectorAll('span')[adb3]) {
-                if (document.querySelectorAll('span')[adb3].innerText.indexOf('ncios detectado') >= 0 && document.querySelectorAll('span')[adb3].parentElement.parentElement.parentElement.parentElement.style.display != 'none') {
+                if (document.querySelectorAll('span')[adb3].innerText.toLowerCase().indexOf('ncios detectado') >= 0 && document.querySelectorAll('span')[adb3].parentElement.parentElement.parentElement.parentElement.style.display != 'none') {
                     document.querySelectorAll('span')[adb3].parentElement.parentElement.parentElement.parentElement.parentElement.style.display = 'none';
                     //var temp = document.querySelectorAll('span')[adb3]; temp.parentElement.parentElement.parentElement.parentElement.parentElement.removeChild(temp);
                     console.log('dailymotion');
@@ -76,9 +80,16 @@
 
         for (var adb4 = 0; adb4 < document.querySelectorAll('div').length; adb4++) {
             if (document.querySelectorAll('div')[adb4]) {
-                if (document.querySelectorAll('div')[adb4].innerText.indexOf('Adblock') > 0) {
+                if (document.querySelectorAll('div')[adb4].innerText.toLowerCase().indexOf('adblock') > 0) {
                     document.querySelectorAll('div')[adb4].style.display = 'none';
                     console.log('adb4');
+                    quebrascroll();
+                }
+            }
+            if (document.querySelectorAll('div')[adb4]) {
+                if (document.querySelectorAll('div')[adb4].innerText.toLowerCase().indexOf('bloqueador') > 0) {
+                    document.querySelectorAll('div')[adb4].style.display = 'none';
+                    console.log('adb5');
                     quebrascroll();
                 }
             }
@@ -102,21 +113,31 @@
             document.body.style.filter = '';
             document.body.style.webkitFilter = '';
             for (var sscript = 0; document.querySelectorAll('script').length > sscript; sscript++) {
-                if (document.querySelectorAll('script')[sscript].innerText.indexOf('Anti-adblock') > 0) {
-                    console.log('sscript1');
-                    document.querySelectorAll('script')[sscript].remove();
+                if (document.querySelectorAll('script')[sscript]) {
+                    if (document.querySelectorAll('script')[sscript].innerText.toLowerCase().indexOf('anti-adblock') > 0) {
+                        console.log('sscript1');
+                        document.querySelectorAll('script')[sscript].remove();
+                    }
                 }
-                if (document.querySelectorAll('script')[sscript].innerText.indexOf('|Adblock') > 0) {
-                    console.log('sscript2');
-                    document.querySelectorAll('script')[sscript].remove();
+                if (document.querySelectorAll('script')[sscript]) {
+                    if (document.querySelectorAll('script')[sscript].innerText.toLowerCase().indexOf('|adblock') > 0) {
+                        console.log('sscript2');
+                        document.querySelectorAll('script')[sscript].remove();
+                    }
+                }
+                if (document.querySelectorAll('script')[sscript]) {
+                    if (document.querySelectorAll('script')[sscript].innerText.toLowerCase().indexOf('blocker') > 0) {
+                        console.log('sscript3');
+                        document.querySelectorAll('script')[sscript].remove();
+                    }
                 }
             }
             for (var sstyle = 0; document.querySelectorAll('style').length > sstyle; sstyle++) {
-                if (document.querySelectorAll('style')[sstyle].innerText.indexOf('Anti-adblock') > 0) {
+                if (document.querySelectorAll('style')[sstyle].innerText.toLowerCase().indexOf('anti-adblock') > 0) {
                     console.log('sstyle1', sstyle);
                     document.querySelectorAll('style')[sstyle].remove();
                 }
-                if (document.querySelectorAll('style')[sstyle].innerText.indexOf('blur') > 0) {
+                if (document.querySelectorAll('style')[sstyle].innerText.toLowerCase().indexOf('blur') > 0) {
                     console.log('sstyle2', sstyle);
                     document.querySelectorAll('style')[sstyle].innerText = document.querySelectorAll('style')[sstyle].innerText.replaceAll('blur', 'xlur');
                     //document.querySelectorAll('style')[sstyle].remove();
