54f436bdc5025f76222a29dca76d6d39caffe78c
Sandro Alvares <sandro.rider@gmail.com>
2023-09-19 16:46:45 -0300

    Update PixLR Editor - Ads Removed.user.js

--- PixLR Editor - Ads Removed.user.js
+++ PixLR Editor - Ads Removed.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         PixLR Editor - Ads Removed
 // @namespace    http://www.sandroalvares.com.br
-// @version      0.35
-// @description  PixLR Editor - Ads Removed
+// @version      0.37
+// @description  PixLR Editor - Ads Removed + Free Save IMG
 // @author       Sandro Alvares
-// @match        http*://pixlr.com/br/e/
+// @match        http*://pixlr.com/br/e/*
 // @grant        none
 
 // @updateURL    https://github.com/KingRider/userscripts/raw/master/PixLR%20Editor%20-%20Ads%20Removed.user.js
@@ -14,7 +14,39 @@
 
 (function() {
 	//$('body').removeProp();
-	var tempo = setInterval(function() {
+
+    var tempo1 = setInterval(function() {
+
+        if (document.querySelector('section#top-bar ul#menu li')) {
+            var myBodyIdx = document.querySelector('section#top-bar ul#menu');
+            var newBaitTagx = document.createElement('li');
+            newBaitTagx.classList.add('sandro');
+
+            var newBaitTextx = document.createTextNode('FreeDownload-JPG');
+            newBaitTagx.appendChild(newBaitTextx);
+            myBodyIdx.appendChild(newBaitTagx);
+
+            // Convert canvas to image
+            document.querySelector('section#top-bar ul#menu li.sandro').addEventListener("click", function(e) {
+                var canvas = document.querySelectorAll('canvas')[0];
+                var dataURL = canvas.toDataURL("image/jpeg", 1.0);
+
+                downloadImage(dataURL, 'my-canvas.jpeg');
+            });
+
+            // Save | Download image
+            function downloadImage(data, filename = 'untitled.jpeg') {
+                var a = document.createElement('a');
+                a.href = data;
+                a.download = filename;
+                document.body.appendChild(a);
+                a.click();
+            }
+            clearInterval(tempo1);
+        }
+	}, 1000);
+
+	var tempo2 = setInterval(function() {
 		/*
         if ($('body').css('padding-right')) {
 			$('body').css('padding-right', 'initial').css('min-height','0px').css('margin','0');
@@ -24,8 +56,10 @@
         if (document.querySelectorAll('div#right\-space').length > 0) {
             document.querySelectorAll('div#right\-space')[0].style.display = 'none';
             document.querySelectorAll('body div#workspace')[0].style.right = 0;
-            clearInterval(tempo);
+            clearInterval(tempo2);
         }
 		//if ($('iframe').length > 0) { $('iframe').remove(); }
+
 	}, 1000);
+
 })();
