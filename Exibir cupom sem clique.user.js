b84439911c3e44d43c297b659342fe5c91d42f09
Sandro Alvares <sandro.rider@gmail.com>
2025-12-31 14:02:55 -0300

    Update Exibir cupom sem clique.user.js
    new update filter pelando.com.br

--- Exibir cupom sem clique.user.js
+++ Exibir cupom sem clique.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Exibir cupom sem clique
 // @namespace    http://sandroalvares.com.br
-// @version      v0.9
+// @version      v0.93
 // @description  Menos clique e menos popup e muito menos propaganda
 // @author       Sandro Alvares
-// @match        http*://*.cuponomia.com.br/*
-// @match        http*://*.radarcupom.com.br/*
-// @match        http*://*.pelando.com.br/cupons-de-descontos/*
-// @match        http*://thiagorodrigo.com.br/cupom-*/*
-// @match        http*://www.meliuz.com.br/desconto/*
+// @match        *://*.cuponomia.com.br/*
+// @match        *://*.radarcupom.com.br/*
+// @match        *://*.pelando.com.br/cupons-de-descontos/*
+// @match        *://thiagorodrigo.com.br/cupom-*/*
+// @match        *://www.meliuz.com.br/desconto/*
 // @grant        none
 
 // @updateURL    https://github.com/KingRider/userscripts/raw/master/Exibir%20cupom%20sem%20clique.user.js
@@ -37,26 +37,20 @@
 
         //--------------------------------------------
         // Pelando.com.br
-        for (var pelando=0; pelando < document.querySelectorAll('span[role="presentation"]').length; pelando++) {
-            document.querySelectorAll('span[role="presentation"]')[pelando].parentElement.parentNode.append(document.querySelectorAll('span[role="presentation"]')[pelando].getAttribute('data-masked'));
-        }
-        /*
-        for (y=0; y < document.querySelectorAll('span.item\-promo\-link.item-cover').length; y++) {
-            document.querySelectorAll('span.item\-promo\-link.item-cover')[y].style.display = "none";
-        }
-        /*
-        for (var y2=0; y2 < document.querySelectorAll('button span').length; y2++) {
-            if (document.querySelectorAll('button span')[y2].nextSibling) {
-                if (document.querySelectorAll('button span')[y2].nextSibling.masked) {
-                    texto = document.querySelectorAll('button span')[y2].nextSibling.dataset.masked;
-                    document.querySelectorAll('button span')[y2].parentElement.parentElement.append(texto);
-                } else {
-                    texto = '';
-                }
+        for (var pelando=0; pelando < document.querySelectorAll('div.coupon-action > a').length; pelando++) {
+            if (document.querySelectorAll('div.coupon\-action > a')[pelando].innerText.indexOf("Ver") >= 0) {
+                document.querySelectorAll('div.coupon\-action > a')[pelando].parentElement.parentElement.style.display = "none";
             }
+            /*if (document.querySelectorAll('div.coupon-action > a')[pelando].innerText.indexOf("grupo") >= 0) {
+                document.querySelectorAll('div.coupon-action > a')[pelando].parentElement.parentElement.style.display = "none";
+            }*/
+            const cupom = document.querySelectorAll('div.coupon-action > a')[pelando].getAttribute('data-coupon-code');
+            document.querySelectorAll('div.coupon-action > a')[pelando].parentElement.parentNode.append(cupom);
+            for (var pelando2=0; pelando2 < document.querySelectorAll('div.deal\-item').length; pelando2++) {
+                document.querySelectorAll('div.deal\-item')[pelando2].parentElement.style.display = "none";
+            }
+            document.querySelector("div.coupon\-group\-content").parentElement.style.display = "none";
         }
-        */
-
         //--------------------------------------------
         // thiagorodrigo.com.br
         for (var throdrigo = 0; throdrigo < document.querySelectorAll('div p.coupon-title-tr').length; throdrigo++) {
